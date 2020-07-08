9119f7b29b554a117eacd34f7c4979eb0d37403d
Roberto <betocostaf@gmail.com>
2020-07-08 15:19:48 -0300

    Drawing only starting and end point

--- 02-dfs/sketch.js
+++ 02-dfs/sketch.js
@@ -1,86 +1,204 @@
 // Daniel Shiffman
 // http://codingtra.in
 // http://patreon.com/codingtrain
+// Part 1: https://youtu.be/aKYlikFAV4k
+// Part 2: https://youtu.be/EaZxUCWAjb0
+// Part 3: https://youtu.be/jwRT4PCT6RU
 
-// Videos
-// https://youtu.be/HyK_Q5rrcr4
-// https://youtu.be/D8UgRyRnvXU
-// https://youtu.be/8Ju_uxJ9v44
-// https://youtu.be/_p5IH0L63wo
-
-// Depth-first search
-// Recursive backtracker
-// https://en.wikipedia.org/wiki/Maze_generation_algorithm
-
-let cols, rows;
-let w = 20;
-let grid = [];
-let current;
-let stack = [];
-
-function setup() {
-  createCanvas(600, 600);
-  cols = floor(width / w);
-  rows = floor(height / w);
-
-  for (let j = 0; j < rows; j++) {
-    for (let i = 0; i < cols; i++) {
-      var cell = new Cell(i, j);
-      grid.push(cell);
+// Function to delete element from the array
+function removeFromArray(arr, elt) {
+    // Could use indexOf here instead to be more efficient
+    for (var i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] == elt) {
+        arr.splice(i, 1);
+      }
     }
   }
-
-  current = grid[0];
-}
-
-function draw() {
-  background(51);
-  for (let i = 0; i < grid.length; i++) {
-    grid[i].show();
+  
+  // An educated guess of how far it is between two points
+  function heuristic(a, b) {
+    var d = dist(a.i, a.j, b.i, b.j);
+    // var d = abs(a.i - b.i) + abs(a.j - b.j);
+    return d;
+  }
+  
+  // How many columns and rows?
+  var cols = 10;
+  var rows = 10;
+  
+  // This will be the 2D array
+  var grid = new Array(cols);
+  
+  // Open and closed set
+  var openSet = [];
+  var closedSet = [];
+  
+  // Start and end
+  var start;
+  var end;
+  
+  // Width and height of each cell of grid
+  var w, h;
+  
+  // The road taken
+  var path = [];
+  
+  function setup() {
+    createCanvas(400, 400);
+  
+    // Grid cell size
+    w = width / cols;
+    h = height / rows;
+  
+    // Making a 2D array
+    for (var i = 0; i < cols; i++) {
+      grid[i] = new Array(rows);
+    }
+  
+    for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
+        grid[i][j] = new Spot(i, j);
+      }
+    }
+  
+    // All the neighbors
+    for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
+        grid[i][j].addNeighbors(grid);
+      }
+    }
+  
+  
+    // Start and end
+    start = grid[0][0]; // TODO: Posicao aleatoria
+    end = grid[cols - 1][rows - 1]; // TODO: Posicao aleatoria
+    start.wall = false;
+    end.wall = false;
+  
+    // openSet starts with beginning only
+    openSet.push(start);
+  }
+  
+  function draw() {
+  
+    // Am I still searching?
+    if (openSet.length > 0) {
+  
+      // Best next option
+      var winner = 0;
+      for (var i = 0; i < openSet.length; i++) {
+        if (openSet[i].g < openSet[winner].g) {
+          winner = i;
+        }
+      }
+      var current = openSet[winner];
+  
+      // Did I finish?
+      if (current === end) {
+        noLoop();
+        console.log("DONE!");
+      }
+  
+      // Best option moves from openSet to closedSet
+      removeFromArray(openSet, current);
+      closedSet.push(current);
+  
+      // Check all the neighbors
+      var neighbors = current.neighbors;
+      for (var i = 0; i < neighbors.length; i++) {
+        var neighbor = neighbors[i];
+  
+        // Valid next spot?
+        if (!closedSet.includes(neighbor) && !neighbor.wall) {
+          var tempG = current.g + heuristic(neighbor, current);
+  
+          // Is this a better path than before?
+          var newPath = false;
+          if (openSet.includes(neighbor)) {
+            if (tempG < neighbor.g) {
+              neighbor.g = tempG;
+              newPath = true;
+            }
+          } else {
+            neighbor.g = tempG;
+            newPath = true;
+            openSet.push(neighbor);
+          }
+  
+          // Yes, it's a better path
+          if (newPath) {
+            neighbor.h = heuristic(neighbor, end);
+            neighbor.f = neighbor.g + neighbor.h;
+            neighbor.previous = current;
+          }
+        }
+  
+      }
+      // Uh oh, no solution
+    } else {
+      console.log('no solution');
+      noLoop();
+      return;
+    }
+  
+    // Draw current state of everything
+    background(255);
+  
+    for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
+        grid[i][j].show();
+      }
+    }
+  
+    // for (var i = 0; i < closedSet.length; i++) {
+    //   closedSet[i].show(color(255, 0, 0, 50));
+    // }
+  
+    // for (var i = 0; i < openSet.length; i++) {
+    //   openSet[i].show(color(0, 255, 0, 50));
+    // }
+  
+  
+    // Find the path by working backwards
+    path = [];
+    var temp = current;
+    path.push(temp);
+    path.push(temp);
+    // while (temp.previous) {
+    //   path.push(temp.previous);
+    //   temp = temp.previous;
+    // }
+  
+  
+    // for (var i = 0; i < path.length; i++) {
+    // path[i].show(color(0, 0, 255));
+    //}
+  
+    // Drawing path as continuous line
+    noFill();
+    stroke(255, 0, 200);
+    strokeWeight(w / 2);
+    beginShape();
+    // vertex(path[path.length-1].i * w + w / 2, path[path.length-1].j * h + h / 2);
+    for (var i = 0; i < path.length; i++) {
+        vertex(path[i].i * w + w / 2, path[i].j * h + h / 2);
+    }
+    endShape();
+    beginShape();
+    stroke(0, 255, 200);
+    vertex(end.i * w + w / 2, end.j * h + h / 2);
+    vertex(end.i * w + w / 2, end.j * h + h / 2);
+    endShape();
+    sleep(200);
+  
+  
+  
   }
 
-  current.visited = true;
-  current.highlight();
-  // STEP 1
-  let next = current.checkNeighbors();
-  if (next) {
-    next.visited = true;
-
-    // STEP 2
-    stack.push(current);
-
-    // STEP 3
-    removeWalls(current, next);
-
-    // STEP 4
-    current = next;
-  } else if (stack.length > 0) {
-    current = stack.pop();
+  function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
   }
-}
-
-function index(i, j) {
-  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
-    return -1;
-  }
-  return i + j * cols;
-}
-
-function removeWalls(a, b) {
-  let x = a.i - b.i;
-  if (x === 1) {
-    a.walls[3] = false;
-    b.walls[1] = false;
-  } else if (x === -1) {
-    a.walls[1] = false;
-    b.walls[3] = false;
-  }
-  let y = a.j - b.j;
-  if (y === 1) {
-    a.walls[0] = false;
-    b.walls[2] = false;
-  } else if (y === -1) {
-    a.walls[2] = false;
-    b.walls[0] = false;
-  }
-}
