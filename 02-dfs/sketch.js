6ac189f2c5a30897003d0c4b1f7618d85de27ea5
Roberto <betocostaf@gmail.com>
2020-07-08 16:30:55 -0300

    Organizing code

--- 02-dfs/sketch.js
+++ 02-dfs/sketch.js
@@ -5,170 +5,145 @@
 // Part 2: https://youtu.be/EaZxUCWAjb0
 // Part 3: https://youtu.be/jwRT4PCT6RU
 
-// Function to delete element from the array
-function removeFromArray(arr, elt) {
-    // Could use indexOf here instead to be more efficient
-    for (var i = arr.length - 1; i >= 0; i--) {
-      if (arr[i] == elt) {
-        arr.splice(i, 1);
-      }
+// How many columns and rows?
+var cols = 10;
+var rows = 10;
+
+// This will be the 2D array
+var grid = new Array(cols);
+
+// Open and closed set
+var openSet = [];
+var closedSet = [];
+
+// Start and end
+var start;
+var end;
+
+// Width and height of each cell of grid
+var w, h;
+
+// The road taken
+var path = [];
+
+function setup() {
+  createCanvas(400, 400);
+
+  // Grid cell size
+  w = width / cols;
+  h = height / rows;
+
+  // Making a 2D array
+  for (var i = 0; i < cols; i++) {
+    grid[i] = new Array(rows);
+  }
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Spot(i, j);
     }
   }
-  
-  // An educated guess of how far it is between two points
-  function heuristic(a, b) {
-    var d = dist(a.i, a.j, b.i, b.j);
-    // var d = abs(a.i - b.i) + abs(a.j - b.j);
-    return d;
+
+  // All the neighbors
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].addNeighbors(grid);
+    }
   }
-  
-  // How many columns and rows?
-  var cols = 10;
-  var rows = 10;
-  
-  // This will be the 2D array
-  var grid = new Array(cols);
-  
-  // Open and closed set
-  var openSet = [];
-  var closedSet = [];
-  
+
+
   // Start and end
-  var start;
-  var end;
-  
-  // Width and height of each cell of grid
-  var w, h;
-  
-  // The road taken
-  var path = [];
-  
-  function setup() {
-    createCanvas(400, 400);
-  
-    // Grid cell size
-    w = width / cols;
-    h = height / rows;
-  
-    // Making a 2D array
-    for (var i = 0; i < cols; i++) {
-      grid[i] = new Array(rows);
-    }
-  
-    for (var i = 0; i < cols; i++) {
-      for (var j = 0; j < rows; j++) {
-        grid[i][j] = new Spot(i, j);
-      }
-    }
-  
-    // All the neighbors
-    for (var i = 0; i < cols; i++) {
-      for (var j = 0; j < rows; j++) {
-        grid[i][j].addNeighbors(grid);
-      }
-    }
-  
-  
-    // Start and end
-    start = grid[0][0]; // TODO: Posicao aleatoria
-    end = grid[cols - 1][rows - 1]; // TODO: Posicao aleatoria
-    start.wall = false;
-    end.wall = false;
-  
-    // openSet starts with beginning only
-    openSet.push(start);
-  }
-  
-  function draw() {
-  
-    // Am I still searching?
-    while (openSet.length > 0) {
-      // Best next option
-      console.log(openSet[0])
-      var current = openSet[0];
-  
-      // Did I finish?
-      if (current === end) {
-        console.log('end')
-        break;
-      }
-  
-      // Best option moves from openSet to closedSet
-      openSet.shift()
-      closedSet.push(current);
-  
-      // Check all the neighbors
-      var neighbors = current.neighbors;
-      for (var i = 0; i < neighbors.length; i++) {
-        var neighbor = neighbors[i];
-  
-        // Valid next spot?
-        if (!closedSet.includes(neighbor) && !openSet.includes(neighbor) && !neighbor.wall) {
-          neighbor.previous = current;
-          openSet.push(neighbor);
-        }
-      }
-      // Uh oh, no solution
+  start = grid[0][0]; // TODO: Posicao aleatoria
+  end = grid[cols - 1][rows - 1]; // TODO: Posicao aleatoria
+  start.wall = false;
+  end.wall = false;
+
+  // openSet starts with beginning only
+  openSet.push(start);
+}
+
+function draw() {
+  // closedSet = []
+
+  // Am I still searching?
+  while (openSet.length > 0) {
+    // Best next option
+    console.log(openSet[0])
+    var current = openSet[0];
+
+    // Did I finish?
+    if (current === end) {
+      console.log('end')
+      break;
     }
 
-    if (current !== end) {
-        console.log('no solution');
-        noLoop();
-        return;
-      }
-  
-    // Draw current state of everything
-    background(255);
-  
-    for (var i = 0; i < cols; i++) {
-      for (var j = 0; j < rows; j++) {
-        grid[i][j].show();
+    // Best option moves from openSet to closedSet
+    openSet.shift()
+    closedSet.push(current);
+
+    // Check all the neighbors
+    var neighbors = current.neighbors;
+    for (var i = 0; i < neighbors.length; i++) {
+      var neighbor = neighbors[i];
+
+      // Valid next spot?
+      if (!closedSet.includes(neighbor) && !openSet.includes(neighbor) && !neighbor.wall) {
+        neighbor.previous = current;
+        openSet.push(neighbor);
       }
     }
-  
-    // Find the path by working backwards
-    path = [];
-    var temp = current;
-    while (temp.previous) {
-      path.push(temp.previous);
-      temp = temp.previous;
-    }
-    path.reverse();
-    start = path[1]
-    openSet = []
-    // closedSet = []
-    openSet.push(start);
-  
-    // for (var i = 0; i < path.length; i++) {
-    // path[i].show(color(0, 0, 255));
-    //}
-  
-    // Drawing path as continuous line
-    noFill();
-    stroke(255, 0, 200);
-    strokeWeight(w / 2);
-    beginShape();
-    vertex(path[0].i * w + w / 2, path[0].j * h + h / 2);
-    vertex(path[0].i * w + w / 2, path[0].j * h + h / 2);
-    // for (var i = 0; i < path.length; i++) {
-    //     vertex(path[i].i * w + w / 2, path[i].j * h + h / 2);
-    // }
-    endShape();
-    beginShape();
-    stroke(0, 255, 200);
-    vertex(end.i * w + w / 2, end.j * h + h / 2);
-    vertex(end.i * w + w / 2, end.j * h + h / 2);
-    endShape();
-    // sleep(200);
-  
-  
-  
+    // Uh oh, no solution
   }
 
-  function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
+  if (current !== end) {
+      console.log('no solution');
+      noLoop();
+      return;
+    }
+
+  // Draw current state of everything
+  background(255);
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
   }
+
+  // Find the path by working backwards
+  path = [];
+  var temp = current;
+  while (temp.previous) {
+    path.push(temp.previous);
+    temp = temp.previous;
+  }
+  path.reverse();
+  start = path[1]
+  openSet = []
+  openSet.push(start);
+
+
+  // Drawing path as continuous line
+  noFill();
+  stroke(255, 0, 200);
+  strokeWeight(w / 2);
+  beginShape();
+  vertex(path[0].i * w + w / 2, path[0].j * h + h / 2);
+  vertex(path[0].i * w + w / 2, path[0].j * h + h / 2);
+  endShape();
+  
+  beginShape();
+  stroke(0, 255, 200);
+  vertex(end.i * w + w / 2, end.j * h + h / 2);
+  vertex(end.i * w + w / 2, end.j * h + h / 2);
+  endShape();
+  // sleep(200);
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
