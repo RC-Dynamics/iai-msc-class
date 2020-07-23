547f4f16715dadc13f5728f182bea9d58b9e8965
Lucas Cavalcanti <lhcs@cin.ufpe.br>
2020-07-23 08:15:58 -0300

    Printing additional informations

--- 05-greedy/sketch.js
+++ 05-greedy/sketch.js
@@ -30,7 +30,10 @@ var path = [];
 let total_f = 0;
 
 function setup() {
-  createCanvas(600, 600);
+  let canvas = createCanvas(600, 600);
+  canvas.position(150, 30);
+
+  printStates();
 
   // Grid cell size
   w = width / cols;
@@ -257,4 +260,35 @@ function compare (a, b) {
     return 1;
   }
     return 0;
+}
+
+function printStates(){
+  let myDiv0 = createDiv('Vehicle');
+  let myDiv1 = createDiv('Food');
+  let myDiv2 = createDiv('ClosedSet');
+  let myDiv3 = createDiv('OpenSet');
+  myDiv0.position(10, 50);
+  myDiv0.style("font-family", "monospace");
+  myDiv0.style("font-size", "12pt");
+  myDiv0.style("padding", "10px");
+  myDiv0.style("background-color", "#FF64C8");
+  myDiv0.style("color", "#000000");
+  myDiv1.position(10, 100);
+  myDiv1.style("font-family", "monospace");
+  myDiv1.style("font-size", "12pt");
+  myDiv1.style("padding", "10px");
+  myDiv1.style("background-color", "#00FF00");
+  myDiv1.style("color", "#000000");
+  myDiv2.position(10, 150);
+  myDiv2.style("font-family", "monospace");
+  myDiv2.style("font-size", "12pt");
+  myDiv2.style("padding", "10px");
+  myDiv2.style("background-color", "rgba(0, 50, 255, 0.2)");
+  myDiv2.style("color", "#000000");
+  myDiv3.position(10, 200);
+  myDiv3.style("font-family", "monospace");
+  myDiv3.style("font-size", "12pt");
+  myDiv3.style("padding", "10px");
+  myDiv3.style("background-color", "rgba(0, 0, 255, 0.47)");
+  myDiv3.style("color", "#000000");
 }
